bb821002cd77086bcbeb75446ec43b7edd1eeb4c
benkaan001 <benkaan001@gmail.com>
2022-01-11 12:55:42 -0600

    Complete testing for reducers

--- client/src/__tests__/reducers.test.js
+++ client/src/__tests__/reducers.test.js
@@ -6,6 +6,12 @@ import {
   UPDATE_CATEGORIES,
   UPDATE_PRODUCTS,
   UPDATE_CURRENT_CATEGORY,
+  ADD_TO_CART,
+  ADD_MULTIPLE_TO_CART,
+  REMOVE_FROM_CART,
+  TOGGLE_CART,
+  CLEAR_CART,
+  UPDATE_CART_QUANTITY,
 } from '../utils/actions';
 
 // sample of the global object
@@ -13,6 +19,19 @@ const initialState = {
   products: [],
   categories: [{ name: 'Watch' }],
   currentCategory: 'Watch',
+  cart: [
+    {
+      _id: '1',
+      name: 'Watch',
+      purchaseQuantity: 1,
+    },
+    {
+      _id: '2',
+      name: 'Book',
+      purchaseQuantity: 2,
+    },
+  ],
+  cartOpen: false,
 };
 
 // test if a product can be added to the products array
@@ -43,4 +62,81 @@ test('UPDATE_CURRENT_CATEGORY', () => {
   });
   expect(newState.currentCategory).toBe('Shoes');
   expect(initialState.currentCategory).toBe('Watch');
+});
+
+test('ADD_TO_CART', () => {
+  let newState = reducer(initialState, {
+    type: ADD_TO_CART,
+    product: { purchaseQuantity: 1 },
+  });
+  expect(newState.cart.length).toBe(3);
+  expect(initialState.cart.length).toBe(2);
+});
+
+test('ADD_MULTIPLE_TO_CART', () => {
+  let newState = reducer(initialState, {
+    type: ADD_MULTIPLE_TO_CART,
+    products: [{}, {}],
+  });
+  expect(newState.cart.length).toBe(4);
+  expect(initialState.cart.length).toBe(2);
+});
+
+test('REMOVE_FROM_CART', () => {
+  let newState1 = reducer(initialState, {
+    type: REMOVE_FROM_CART,
+    _id: '1',
+  });
+  // cart is still open
+  expect(newState1.cartOpen).toBe(true);
+
+  // the second item should now be the firt
+  expect(newState1.cart.length).toBe(1);
+  expect(newState1.cart[0]._id).toBe('2');
+
+  let newState2 = reducer(newState1, {
+    type: REMOVE_FROM_CART,
+    _id: '2',
+  });
+
+  // cart is empty and closed
+  expect(newState2.cartOpen).toBe(false);
+  expect(newState2.cart.length).toBe(0);
+
+  expect(initialState.cart.length).toBe(2);
+});
+
+test('UPDATE_CART_QUANTITY', () => {
+  let newState = reducer(initialState, {
+    type: UPDATE_CART_QUANTITY,
+    _id: '1',
+    purchaseQuantity: 3,
+  });
+  expect(newState.cartOpen).toBe(true);
+  expect(newState.cart[0].purchaseQuantity).toBe(3);
+  expect(newState.cart[1].purchaseQuantity).toBe(2);
+
+  expect(initialState.cartOpen).toBe(false);
+});
+
+test('CLEAR_CART', () => {
+  let newState = reducer(initialState, {
+    type: CLEAR_CART,
+  });
+  expect(newState.cartOpen).toBe(false);
+  expect(newState.cart.length).toBe(0);
+  expect(initialState.cart.length).toBe(2);
+});
+
+test('TOGGLE_CART', () => {
+  let newState = reducer(initialState, {
+    type: TOGGLE_CART,
+  });
+  expect(newState.cartOpen).toBe(true);
+  expect(initialState.cartOpen).toBe(false);
+
+  let newState2 = reducer(newState, {
+    type: TOGGLE_CART,
+  });
+  expect(newState2.cartOpen).toBe(false);
 });
--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -4,6 +4,12 @@ import {
   UPDATE_PRODUCTS,
   UPDATE_CATEGORIES,
   UPDATE_CURRENT_CATEGORY,
+  ADD_TO_CART,
+  ADD_MULTIPLE_TO_CART,
+  REMOVE_FROM_CART,
+  TOGGLE_CART,
+  CLEAR_CART,
+  UPDATE_CART_QUANTITY,
 } from './actions';
 
 export const reducer = (state, action) => {
@@ -18,6 +24,49 @@ export const reducer = (state, action) => {
       return { ...state, categories: [...action.categories] };
     case UPDATE_CURRENT_CATEGORY:
       return { ...state, currentCategory: action.currentCategory };
+    case ADD_TO_CART:
+      return {
+        ...state,
+        cartOpen: true,
+        cart: [...state.cart, action.product],
+      };
+    case ADD_MULTIPLE_TO_CART:
+      return {
+        ...state,
+        cartOpen: true,
+        cart: [...state.cart, ...action.products],
+      };
+    case REMOVE_FROM_CART:
+      let newState = state.cart.filter((product) => {
+        return product._id !== action._id;
+      });
+      return {
+        ...state,
+        cartOpen: newState.length > 0,
+        cart: newState,
+      };
+    case UPDATE_CART_QUANTITY:
+      return {
+        ...state,
+        cartOpen: true,
+        cart: state.cart.map((product) => {
+          if (action._id === product._id) {
+            product.purchaseQuantity = action.purchaseQuantity;
+          }
+          return product;
+        }),
+      };
+    case CLEAR_CART:
+      return {
+        ...state,
+        cartOpen: false,
+        cart: [],
+      };
+    case TOGGLE_CART:
+      return {
+        ...state,
+        cartOpen: !state.cartOpen,
+      };
     // if action type is none of these, do not update the state
     default:
       return state;
