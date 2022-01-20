897a66211fd65f5a21f695c3c484836383a2d89d
benkaan001 <benkaan001@gmail.com>
2022-01-20 01:59:20 -0600

    Heroku Deployment

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -16,77 +16,78 @@ db.once('open', async () => {
 
   const products = await Product.insertMany([
     {
-      name: 'Mens Item I',
-      description: 'Add description here',
+      name: 'Mens Item 1',
+      description:
+        'Duis dignissim nisi vel tortor imperdiet finibus. Aenean aliquam sagittis rutrum.',
       image: 'mens1.png',
       category: categories[0]._id,
       price: 49.99,
       quantity: 100,
     },
     {
-      name: 'Mens Item II',
-      description: 'Add description here',
+      name: 'Mens Item 2',
+      description:
+        'Donec turpis odio, semper vel interdum ut, vulputate at ex. Duis dignissim nisi vel tortor imperdiet finibus. Aenean aliquam sagittis rutrum.',
       image: 'mens2.png',
       category: categories[0]._id,
       price: 39.99,
       quantity: 200,
     },
     {
-      name: 'Mens Item III',
+      name: 'Mens Item 3',
       category: categories[0]._id,
       description:
-        'Phasellus tempus euismod urna. Proin ultrices nisi ut ipsum congue, vitae porttitor libero suscipit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam lacinia a nisi non congue.',
+        'Phasellus tempus euismod urna. Proin ultrices nisi ut ipsum congue, vitae porttitor libero suscipit. ',
       image: 'mens3.png',
       price: 79.99,
       quantity: 100,
     },
     {
-      name: 'Mens Item IV',
+      name: 'Mens Item 4',
       category: categories[0]._id,
       description:
         'Lectus arcu laoreet felis, et fringilla sapien turpis vestibulum nisl.',
       image: 'mens4.png',
-      price: 99.99,
+      price: 49.99,
       quantity: 200,
     },
     {
-      name: 'Womens Item I',
+      name: 'Womens Item 1',
       category: categories[1]._id,
       description:
-        'Donec turpis odio, semper vel interdum ut, vulputate at ex. Duis dignissim nisi vel tortor imperdiet finibus. Aenean aliquam sagittis rutrum.',
+        'Donec turpis odio, semper vel interdum ut, vulputate at ex. Duis dignis imperdiet finibus.',
       image: 'womens1.png',
       price: 29.99,
       quantity: 300,
     },
     {
-      name: 'Womens Item II',
+      name: 'Womens Item 2',
       category: categories[1]._id,
       description:
-        ' Nullam vitae lobortis ligula, ut sagittis massa. Curabitur consectetur, tellus at pulvinar venenatis, erat augue cursus erat, eu ullamcorper eros lectus ultrices ipsum. Integer rutrum, augue vitae auctor venenatis, turpis turpis elementum orci, at sagittis risus mi a leo.',
+        ' Nullam vitae lobortis ligula, ut sagittis massa. Curabitur consectetur, at sagittis risus mi a leo.',
       image: 'womens2.png',
       price: 69.99,
       quantity: 100,
     },
     {
-      name: 'Womens Item III',
+      name: 'Womens Item 3',
       category: categories[1]._id,
       description:
-        'Eget venenatis purus ligula ut nisi. Fusce ut felis dolor. Mauris justo ante, aliquet non tempus in, tempus ac lorem. Aliquam lacinia dolor eu sem eleifend ultrices. Etiam mattis metus metus. Sed ligula dui, placerat non turpis vitae, suscipit volutpat elit. Phasellus sagittis, diam elementum suscipit fringilla, libero mauris scelerisque ex, ac interdum diam erat non sapien.',
+        'Eget venenatis purus ligula ut nisi. Fusce ut felis dolor. Mauris non tempus in, sapien.',
       image: 'womens3.png',
-      price: 79.99,
+      price: 59.99,
       quantity: 200,
     },
     {
-      name: 'Womens Item IV',
+      name: 'Womens Item 4',
       category: categories[1]._id,
-      description:
-        'Vestibulum ornare diam quis eleifend rutrum. Aliquam nulla est, volutpat non enim nec, pharetra gravida augue. Donec vitae dictum neque. Pellentesque arcu lorem, fringilla non ligula ac, tristique bibendum erat. Ut a semper nibh. Quisque a mi et mi tempor ultricies. Maecenas eu ipsum eu enim hendrerit accumsan at euismod urna.',
+      description: 'Vestibulum ornare diam quis eleifend rutrumsmod urna.',
       image: 'womens4.png',
-      price: 89.99,
+      price: 49.99,
       quantity: 100,
     },
     {
-      name: 'Womens Item V',
+      name: 'Womens Item 5',
       category: categories[1]._id,
       description: 'Vulputate hendrerit nibh, a placerat elit cursus interdum.',
       image: 'womens5.png',
@@ -94,7 +95,7 @@ db.once('open', async () => {
       quantity: 100,
     },
     {
-      name: 'Unisex Item I',
+      name: 'Unisex Item 1',
       category: categories[2]._id,
       description:
         'Phasellus lobortis leo odio, sit amet pharetra turpis porta quis.',
@@ -103,16 +104,15 @@ db.once('open', async () => {
       quantity: 100,
     },
     {
-      name: 'Unisex Item II',
+      name: 'Unisex Item 2',
       category: categories[2]._id,
-      description:
-        'Ut laoreet tellus sit amet justo bibendum ultrices. Donec vitae felis vestibulum, congue augue eu, finibus turpis.',
+      description: 'Ut laoreet tellus sit amet justo bibendum ultrices.',
       image: 'unisex2.png',
       price: 49.99,
       quantity: 200,
     },
     {
-      name: 'Unisex Item III',
+      name: 'Unisex Item 3',
       category: categories[2]._id,
       description:
         'Suspendisse potenti. Donec at dui ac sapien eleifend hendrerit vel sit amet lectus.',
