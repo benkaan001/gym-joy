32afabcf9344b558bb0e9fa96988efe68b28ade8
benkaan001 <benkaan001@gmail.com>
2022-01-19 22:13:38 -0600

    update style for Success

--- client/src/pages/Success.js
+++ client/src/pages/Success.js
@@ -1,8 +1,34 @@
 import React, { useEffect } from 'react';
 import { useMutation } from '@apollo/client';
-import Jumbotron from '../components/Jumbotron';
 import { ADD_ORDER } from '../utils/mutations';
 import { idbPromise } from '../utils/helpers';
+import styled from 'styled-components';
+import { mobile } from '../mobileScreen';
+
+const Container = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
+  height: 60vh;
+  ${mobile({ height: '30vh' })};
+`;
+
+const Header = styled.h1`
+  font-weight: 400;
+  ${mobile({ fontSize: '20px' })};
+`;
+
+const Header2 = styled.h2`
+  font-weight: 400;
+  ${mobile({ fontSize: '16px' })};
+`;
+
+const Header3 = styled.h3`
+  font-weight: 400;
+  ${mobile({ fontSize: '14px' })};
+`;
 
 function Success() {
   const [addOrder] = useMutation(ADD_ORDER);
@@ -23,7 +49,7 @@ function Success() {
 
       setTimeout(() => {
         window.location.assign('/');
-      }, 2000);
+      }, 4000);
     }
 
     saveOrder();
@@ -31,11 +57,11 @@ function Success() {
 
   return (
     <div>
-      <Jumbotron>
-        <h1>Success!</h1>
-        <h2>Thank you for your purchase!</h2>
-        <h2>You will now be redirected to the home page now</h2>
-      </Jumbotron>
+      <Container>
+        <Header>Success!</Header>
+        <Header2>Thank you for your purchase!</Header2>
+        <Header3>You will now be redirected to the home page now</Header3>
+      </Container>
     </div>
   );
 }
